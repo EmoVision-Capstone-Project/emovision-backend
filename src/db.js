bc944db9ba04d1571cd16fe16f551a2d61d4adf8
157-Fadina <fadina.123140157@student.itera.ac.id>
2026-04-30 21:30:41 +0700

    update: database connection configuration to use environment variables

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config(); 
 
 const pool = new Pool({
-  user: 'postgres', 
-  host: 'localhost',
-  database: 'emovision_db',
-  password: '123', 
-  port: 5432, 
+  user: process.env.DB_USER, 
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT, 
 });
 
 pool.connect((err, client, release) => {
