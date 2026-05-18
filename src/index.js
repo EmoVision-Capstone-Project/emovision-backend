1698b2b9053459d05d712f9e10a356c632e97e43
157-Fadina <fadina.123140157@student.itera.ac.id>
2026-05-18 23:36:52 +0700

    update: fix port configuration and initialize Supabase client in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const pool = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+const { createClient } = require('@supabase/supabase-js');
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors());
 app.use(express.json());
