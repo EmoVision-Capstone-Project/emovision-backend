2782354b211994a4f39aa2472554b06c9c8dd629
157-Fadina <fadina.123140157@student.itera.ac.id>
2026-05-19 00:03:41 +0700

    update: clean up database connection code and remove unused Supabase client initialization

--- src/db.js
+++ src/db.js
@@ -1,19 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false 
-  }
+  ssl: { rejectUnauthorized: false }
 });
-
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Gagal terhubung ke database cloud', err.stack);
-  }
-  console.log('Berhasil terhubung ke database EmoVision di Supabase!');
-  release();
-});
-
 module.exports = pool;
--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const pool = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors());
 app.use(express.json());
